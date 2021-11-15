e3aad10877aef488a351f3850b1c8d282098bb80
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-15 17:10:09 +0000

    WIP writing tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,10 +1,13 @@
 
-describe fizzBuzz do
-    it("should throw an exception if song is already playing", function() {
-        fizzBuzz.play(song);
-
-        expect(function() {
-            fizzBuzz.resume();
-        }).toThrowError("song is already playing");
+describe ("FizzBuzz", =>
+{
+let fizzBuzz
+}
+    beforeEach(function() {
+        player = new Player();
+        song = new Song();
     });
-end
+
+    it("should be able to play a Song", function() {
+        player.play(song);
+        expect(player.currentlyPlayingSong).toEqual(song);
